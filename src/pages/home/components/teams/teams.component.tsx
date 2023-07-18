8772311fdb1f16300d5a2505e754ae50e9d34342
bishal giri <bishal.giri@ensue.us>
2023-07-18 22:31:58 +0545

    slider added

--- src/pages/home/components/teams/teams.component.tsx
+++ src/pages/home/components/teams/teams.component.tsx
@@ -1,35 +1,41 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import UserCard from "../../../../components/user-card/userCard.component";
 
 import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "./teams.styles.scss";
-import UserCard from "../../../../components/user-card/userCard.component";
-import { Navigation } from "swiper/modules";
-import { Pagination } from "@mui/material";
 
 const Teams: React.FC = (): JSX.Element => {
   return (
-    <Swiper
-      spaceBetween={30}
-      slidesPerView={3}
-      pagination={{ clickable: true }}
-    >
-      <SwiperSlide>
-        <UserCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <UserCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <UserCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <UserCard />
-      </SwiperSlide>
-      <SwiperSlide>
-        <UserCard />
-      </SwiperSlide>
-    </Swiper>
+    <div className="container teams_wrapper">
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={30}
+        slidesPerView={3}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 1500 }}
+      >
+        <SwiperSlide>
+          <UserCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <UserCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <UserCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <UserCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          <UserCard />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
